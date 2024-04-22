5bb29d8f5f6aca9219f1664be74fd7f63a216be7
SHIVAM KUMAR <126109527+SHIVAM1KUMAR@users.noreply.github.com>
2024-04-22 11:54:58 +0530

    Update Arrow fun.js

--- Arrow fun.js
+++ Arrow fun.js
@@ -13,3 +13,36 @@ user.username ="shivam"
 user.welcomemessage()
 
 console.log(this);
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
