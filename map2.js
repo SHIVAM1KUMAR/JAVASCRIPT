c73d8641632252d8d064c176f0f2464d09abc6e3
SHIVAM KUMAR <126109527+SHIVAM1KUMAR@users.noreply.github.com>
2024-04-24 09:41:31 +0530

    Update map2.js

--- map2.js
+++ map2.js
@@ -1,8 +1,8 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+ const newNums = myNumers.map( (num) => { return num + 10})
 
-const newNums = myNumers
+//const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
